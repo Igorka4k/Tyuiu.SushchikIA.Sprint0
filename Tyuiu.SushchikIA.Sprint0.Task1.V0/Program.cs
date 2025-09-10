66f22ed2345437bae1888bac270f2703e846346c
Igorase <garik.sushik@ya.ru>
2025-09-10 21:35:49 +0500

    Add Task1

---
+++ Tyuiu.SushchikIA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SushchikIA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Игорь";
+            int age = 21;
+            Console.WriteLine("Здравствуйюе, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
